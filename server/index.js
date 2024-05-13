a21d09b742e87f7c79c72db01245fe18b6b8f0d1
rowgregory <rowgregory@gmail.com>
2024-05-12 20:48:33 -0400

    chore(render):Update server file to point to the dist folder

--- server/index.js
+++ server/index.js
@@ -51,11 +51,11 @@ app.use('/api/auth', authRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, 'dist')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 }
 
 server.listen(PORT, () => {
